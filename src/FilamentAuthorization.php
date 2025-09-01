515a3b841832d1682a0c87753ea1c6a18d452e50
Timo de Winter <timo@be-interactive.nl>
2025-09-01 16:36:55 +0200

    Adds support for injecting permissions

--- src/FilamentAuthorization.php
+++ src/FilamentAuthorization.php
@@ -2,4 +2,34 @@
 
 namespace TimoDeWinter\FilamentAuthorization;
 
-class FilamentAuthorization {}
+use Illuminate\Support\Arr;
+
+class FilamentAuthorization
+{
+    public array $permissions = [];
+
+    public function registerPermission(string|array $permission, $prefix = 'default', $tab = 'Default'): static
+    {
+        $this->permissions[$tab][$prefix] = [
+            ...($this->permissions[$tab][$prefix] ?? []),
+            ...Arr::wrap($permission),
+        ];
+
+        return $this;
+    }
+
+    public function getTabs(): array
+    {
+        return array_keys($this->permissions);
+    }
+
+    public function getPrefixGroups(string $tab): array
+    {
+        return array_keys($this->permissions[$tab] ?? []);
+    }
+
+    public function getPermissions(string $tab, string $prefix): array
+    {
+        return $this->permissions[$tab][$prefix] ?? [];
+    }
+}
